62ae066a8c19a6b153ed9bc4b16b50273feacfa6
agentcooper <agentcooper@users.noreply.github.com>
2020-01-26 13:52:13 +0000

    Automated deployment: Sun Jan 26 13:52:13 UTC 2020 cacfff53eb6ae636b213981fdd617c20c9274719

---
+++ precache-manifest.b6f9f504bc40a4f71b041c7946e395bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7238793cb66b4c91a9010bb2f87092",
+    "url": "/react-pdf-highlighter/index.html"
+  },
+  {
+    "revision": "84523e348bc0e0c0dbdd",
+    "url": "/react-pdf-highlighter/static/css/2.303b2a07.chunk.css"
+  },
+  {
+    "revision": "40bc20552e7fc721624a",
+    "url": "/react-pdf-highlighter/static/css/main.765757f6.chunk.css"
+  },
+  {
+    "revision": "84523e348bc0e0c0dbdd",
+    "url": "/react-pdf-highlighter/static/js/2.edce721a.chunk.js"
+  },
+  {
+    "revision": "b02398fe302ba4ef3803015f2c370d5c",
+    "url": "/react-pdf-highlighter/static/js/2.edce721a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40bc20552e7fc721624a",
+    "url": "/react-pdf-highlighter/static/js/main.5416f988.chunk.js"
+  },
+  {
+    "revision": "787594290e5e32cf6b12",
+    "url": "/react-pdf-highlighter/static/js/runtime-main.fc987ad8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.191398069231b1d525fb5670b19503c4.js"
+  "/react-pdf-highlighter/precache-manifest.b6f9f504bc40a4f71b041c7946e395bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pdf-highlighter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
