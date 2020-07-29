4cb3be9741a7dcc8259f1d36b2ad3f581ab29084
Artem Tyurin <artem.tyurin@gmail.com>
2020-07-29 15:37:24 +0200

    Apply Prettier

--- packages/react-pdf-highlighter/src/components/PdfHighlighter.js
+++ packages/react-pdf-highlighter/src/components/PdfHighlighter.js
@@ -78,7 +78,7 @@ type Props<T_HT> = {
     content: { text?: string, image?: string },
     hideTipAndSelection: () => void,
     transformSelection: () => void
-  ) =>?React$Element<*>,
+  ) => ?React$Element<*>,
   enableAreaSelection: (event: MouseEvent) => boolean
 };
 
@@ -87,7 +87,7 @@ const EMPTY_ID = "empty-id";
 class PdfHighlighter<T_HT: T_Highlight> extends PureComponent<
   Props<T_HT>,
   State<T_HT>
-  > {
+> {
   state: State<T_HT> = {
     ghostHighlight: null,
     isCollapsed: true,
@@ -362,7 +362,7 @@ class PdfHighlighter<T_HT: T_Highlight> extends PureComponent<
         ...pageViewport.convertToPdfPoint(
           0,
           scaledToViewport(boundingRect, pageViewport, usePdfCoordinates).top -
-          scrollMargin
+            scrollMargin
         ),
         0
       ]
