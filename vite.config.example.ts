defe96adb76020895fd0a127653a84856c6f241c
Artem Tyurin <artem.tyurin@gmail.com>
2021-08-28 19:22:33 +0200

    Fix base URL

--- vite.config.example.ts
+++ vite.config.example.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
+  base: "/react-pdf-highlighter/",
   build: {
     outDir: "dist-example",
   },
