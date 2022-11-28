77ec413604ab4fcfe7e3b120d69b21490ea2dd2c
Vinicius Franco <vinicius.franco@evisort.com>
2022-11-28 09:25:09 -0300

    chore: bump pdf.js version and fix typing compatiblity

--- src/components/PdfLoader.tsx
+++ src/components/PdfLoader.tsx
@@ -28,7 +28,7 @@ export class PdfLoader extends Component<Props, State> {
   };
 
   static defaultProps = {
-    workerSrc: "https://unpkg.com/pdfjs-dist@2.11.338/build/pdf.worker.min.js",
+    workerSrc: "https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js",
   };
 
   documentRef = React.createRef<HTMLElement>();
