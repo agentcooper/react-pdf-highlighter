9820691b48db5bc6542a92c0e1e18190ea73fdc1
Artem Tyurin <artem.tyurin@gmail.com>
2023-07-31 11:18:06 +0200

    Update create-react-app example

--- create-react-app-example/src/index.tsx
+++ create-react-app-example/src/index.tsx
@@ -4,4 +4,4 @@ import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(<App tab="home" />);
+root.render(<App />);
