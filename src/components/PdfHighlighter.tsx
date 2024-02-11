358a754d8b6ee2c31648076c20382e43ba8b2630
Christian de Wit <christian@cleverfranke.com>
2024-02-11 22:52:36 +0100

    Search functionality as per https://github.com/agentcooper/react-pdf-highlighter/pull/185/files

--- src/components/PdfHighlighter.tsx
+++ src/components/PdfHighlighter.tsx
@@ -5,6 +5,7 @@ import "../style/PdfHighlighter.css";
 import {
   EventBus,
   NullL10n,
+  PDFFindController,
   PDFLinkService,
   PDFViewer,
 } from "pdfjs-dist/legacy/web/pdf_viewer";
@@ -71,8 +72,11 @@ interface Props<T_HT> {
   highlights: Array<T_HT>;
   onScrollChange: () => void;
   scrollRef: (scrollTo: (highlight: T_HT) => void) => void;
+  findRefs: (findPrev: () => void, findNext: () => void) => void;
   pdfDocument: PDFDocumentProxy;
   pdfScaleValue: string;
+  searchValue: string;
+  onSearch: (currentMatch: number, totalMatchCount: number) => void;
   onSelectionFinished: (
     position: ScaledPosition,
     content: { text?: string; image?: string },
@@ -90,6 +94,8 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
 > {
   static defaultProps = {
     pdfScaleValue: "auto",
+    searchValue: "",
+    onSearch: () => {},
   };
 
   state: State<T_HT> = {
@@ -140,6 +146,7 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
       const { ownerDocument: doc } = ref;
       eventBus.on("textlayerrendered", this.onTextLayerRendered);
       eventBus.on("pagesinit", this.onDocumentReady);
+      eventBus.on("updatetextlayermatches", this.calculateMatchProgress);
       doc.addEventListener("selectionchange", this.onSelectionChange);
       doc.addEventListener("keydown", this.handleKeyDown);
       doc.defaultView?.addEventListener("resize", this.debouncedScaleValue);
@@ -147,6 +154,7 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
 
       this.unsubscribe = () => {
         eventBus.off("pagesinit", this.onDocumentReady);
+        eventBus.off("updatetextlayermatches", this.calculateMatchProgress);
         eventBus.off("textlayerrendered", this.onTextLayerRendered);
         doc.removeEventListener("selectionchange", this.onSelectionChange);
         doc.removeEventListener("keydown", this.handleKeyDown);
@@ -167,6 +175,13 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
     if (prevProps.highlights !== this.props.highlights) {
       this.renderHighlightLayers();
     }
+    if (prevProps.searchValue != this.props.searchValue) {
+      this.viewer.findController.executeCommand("find", {
+        query: this.props.searchValue,
+        highlightAll: true,
+        phraseSearch: true,
+      });
+    }
   }
 
   init() {
@@ -182,6 +197,10 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
         textLayerMode: 2,
         removePageBorders: true,
         linkService: this.linkService,
+        findController: new PDFFindController({
+          eventBus: this.eventBus,
+          linkService: this.linkService,
+        }),
         l10n: NullL10n,
       });
 
@@ -195,6 +214,44 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
   componentWillUnmount() {
     this.unsubscribe();
   }
+
+  calculateMatchProgress = () => {
+    let totalMatchCount = 0;
+    let currentMatchIndex = 0;
+    let hasCurrentMatchIndex = false;
+    this.viewer.findController.pageMatches.forEach(
+      (pageMatches: number[], pageIndex: number) => {
+        if (
+          !hasCurrentMatchIndex &&
+          pageIndex === this.viewer.findController.selected.pageIdx
+        ) {
+          currentMatchIndex =
+            totalMatchCount + this.viewer.findController.selected.matchIdx;
+          hasCurrentMatchIndex = true;
+        }
+        totalMatchCount += pageMatches.length;
+      }
+    );
+    const currentMatch = totalMatchCount === 0 ? 0 : currentMatchIndex + 1;
+    this.props.onSearch(currentMatch, totalMatchCount);
+  };
+
+  goToNextMatch = () => {
+    const { searchValue } = this.props;
+    this.viewer.findController.executeCommand("findagain", {
+      query: searchValue,
+      highlightAll: true,
+    });
+  };
+
+  goToPreviousMatch = () => {
+    const { searchValue } = this.props;
+    this.viewer.findController.executeCommand("findagain", {
+      query: searchValue,
+      highlightAll: true,
+      findPrevious: true,
+    });
+  };
 
   findOrCreateHighlightLayer(page: number) {
     const { textLayer } = this.viewer.getPageView(page - 1) || {};
@@ -408,11 +465,13 @@ export class PdfHighlighter<T_HT extends IHighlight> extends PureComponent<
   };
 
   onDocumentReady = () => {
-    const { scrollRef } = this.props;
+    const { scrollRef, findRefs } = this.props;
 
     this.handleScaleValue();
 
     scrollRef(this.scrollTo);
+
+    findRefs(this.goToPreviousMatch, this.goToNextMatch);
   };
 
   onSelectionChange = () => {
