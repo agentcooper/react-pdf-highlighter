a412c635e08bfc398d9f4c2751582474fdeb3a04
Artem Tyurin <artem.tyurin@gmail.com>
2024-09-09 23:10:31 +0200

    Preseve modules in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,30 @@
-import ts from "@rollup/plugin-typescript";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
-    cssCodeSplit: true,
+    minify: false,
     lib: {
       entry: "./src/index.ts",
-      name: "ReactPdfHighlighter",
-      fileName: (format) => `react-pdf-highlighter.${format}.js`,
-      formats: ["es", "cjs"],
+      formats: ["es"],
+      fileName: (format, entryName) => `${entryName}.js`,
     },
     rollupOptions: {
-      input: {
-        main: "./src/index.ts",
-        style: "./src/style/index.css",
-      },
-      external: ["react", "react-dom"],
       output: {
-        inlineDynamicImports: false,
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
+        preserveModules: true,
       },
-      plugins: [ts()],
+      external: [
+        "react",
+        "react/jsx-runtime",
+        "react-dom",
+        "react-dom/client",
+        "react-rnd",
+        "pdfjs-dist",
+        "pdfjs-dist/web",
+        "debounce",
+      ],
     },
   },
 });
